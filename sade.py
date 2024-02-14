7dbe5770d33f12d3a453eeb4612655ab84563942
Matias Harju <matias.harju@iki.fi>
2024-02-14 13:54:25 +0200

    update sade.py for HC-SR04 sensor

--- sade.py
+++ sade.py
@@ -10,8 +10,18 @@ import time
 import pygame
 
 GPIO.setmode(GPIO.BCM)
-sensorPin = 17
-GPIO.setup(sensorPin, GPIO.IN)
+
+TRIG = 4
+ECHO = 17
+thresholdDistance = 2.0 # distance for person detection 
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.output(TRIG, False)
+print 'Waiting for sensor to settle...'
+time.sleep(2)
+print 'Sensor ready'
 
 sound = 'rain_umbrella.mp3'
 pygame.mixer.init()
@@ -19,7 +29,24 @@ pygame.mixer.music.load(sound)
 
 try:
     while True:
-        if GPIO.input(sensorPin) == 1 and not pygame.mixer.music.get_busy():
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO)==0:
+            pulse_start = time.time()
+
+        while GPIO.input(ECHO)==1:
+            pulse_end = time.time()
+
+        pulse_duration = pulse_end - pulse_start
+
+        distance = pulse_duration * 17150
+
+        distance = round(distance, 2) # or maybe: (distance + 1.15, 2) ?
+        print ('Distance:', distance, 'cm')
+        
+        if distance < thresholdDistance and not pygame.mixer.music.get_busy():
             pygame.mixer.music.play(-1)
             print ('Start playback and fade-in of', sound)
             for i in range(0, 100):
@@ -27,7 +54,7 @@ try:
 #                if GPIO.input(sensorPin) != 1:
 #                    break                               # Interrupt fade-in if movement not detected anymore
                 time.sleep(0.05)
-        elif GPIO.input(sensorPin) != 1 and pygame.mixer.music.get_busy():
+        elif distance >= thresholdDistance and pygame.mixer.music.get_busy():
             print ('Start fade-out')
             for i in range(100, 0, -1):
                 pygame.mixer.music.set_volume(i / 100)  # Fades out the audio
