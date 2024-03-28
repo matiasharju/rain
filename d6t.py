ac83a81c952e5b3c3836ea5d1e5131c08134103c
Vattu <matias.harju@iki.fi>
2024-03-28 09:59:22 +0200

    now working with 4x4 sensor

--- d6t.py
+++ d6t.py
@@ -20,8 +20,10 @@ import getopt
 import time
 import pigpio
 
+omron_bus = 4             # CHANGE OMRON BUS HERE
 threshold_temp_up = 24.6  # above which sound starts to fade in
 threshold_marginal = 0.2  # substracted from temp_up, used for triggering fade out
+threshold = 5.0             # how many celsius degrees above the reference temperature until triggered
 
 # **** SOUND ****
 sound = '/home/vattu/Documents/rain/rain_umbrella.mp3'
@@ -43,10 +45,9 @@ fade_in_task = None
 fade_out_task = None
 fade_in_triggered = None
 fade_out_triggered = None
-threshold = 1.0             # how many celsius degrees above the reference temperature until triggered
 
 # **** OMRON ****
-i2c_bus = smbus.SMBus(3)
+i2c_bus = smbus.SMBus(omron_bus)
 OMRON_1=0x0a 					# 7 bit I2C address of Omron MEMS Temp Sensor D6T-44L
 OMRON_BUFFER_LENGTH=35				# Omron data buffer size
 temperature_data=[0]*OMRON_BUFFER_LENGTH 	# initialize the temperature data list
@@ -55,16 +56,17 @@ temperature_data=[0]*OMRON_BUFFER_LENGTH 	# initialize the temperature data list
 pi = pigpio.pi()              # use defaults
 version = pi.get_pigpio_version()
 print ('PiGPIO version = '+str(version))
-handle = pi.i2c_open(3, 0x0a) # open Omron D6T device at address 0x0a on bus 1
+handle = pi.i2c_open(omron_bus, 0x0a) # open Omron D6T device at address 0x0a
 
 # initialize the device based on Omron's appnote 1
 result=i2c_bus.write_byte(OMRON_1,0x4c);
 
 # acquire reference temp
 (bytes_read, temperature_data) = pi.i2c_read_device(handle, len(temperature_data))
-reference_temp = (256 * temperature_data[1] + temperature_data[0]) * 0.1
-reference_temp_formatted = "{:.1f}".format(reference_temp) # format to fixed bymber of decimals
-print('Reference temp:', reference_temp_formatted)
+print('temperature_data length:', len(temperature_data))
+sensor_temp = (256 * temperature_data[1] + temperature_data[0]) * 0.1
+sensor_temp_formatted = "{:.1f}".format(sensor_temp) # format to fixed bymber of decimals
+print('Sensor temp:', sensor_temp_formatted)
 
 
 # **** MAIN COROUTINE ****
@@ -106,13 +108,14 @@ async def measure():
         #tRef = sum(lowest_four) / len(lowest_four)          # calculate the average for reference temperature
         #print("Average of the 4 lowest values (tRef):", tRef)
 
-        # use the lowest value for reference temperature
+        # choose the lowest value for reference temperature
         tRef = min(tP)
-        print('Reference temp:', "{:.1f}".format.tRef * 0.1)
+        tHi = max(tP)  # highest value of all sensors
+        print('LOWEST (tRef):', "{:.1f}".format(tRef * 0.1), 'HIGHEST:', "{:.1f}".format(tHi * 0.1))
 
         # format temperatures for printing
         tPF = []    # list of formatted temperatures
-        for i in range(0, tP.__len__):
+        for i in range(0, len(tP)):
             tPF.append("{:.1f}".format(tP[i] * 0.1))     # format to fixed number of decimals
 #        measured_temp_formatted = "{:.1f}".format(measured_temp * 0.1) # format to fixed bymber of decimals
         print(tPF[0], tPF[1], tPF[2], tPF[3], tPF[4], tPF[5], tPF[6], tPF[7], tPF[8], tPF[9], tPF[10], tPF[11], tPF[12], tPF[13], tPF[14], tPF[15])
@@ -146,17 +149,17 @@ async def measure():
 
 
 # **** FADE LOOP ****
-async def fade():
-    current_volume = pygame.mixer.music.get_volume()
-    print('Fade happening...')
-    while (fade_in_triggered):
-        for i in range(int(current_volume) * 100, 100):
-            pygame.mixer.music.set_volume(i/100)
-            await asyncio.sleep(0.05)
-    while (fade_out_triggered):
-        for i in range(int(current_volume) * 100, 0):
-            pygame.mixer.music.set_volume(i/100)
-            await asyncio.sleep(0.05
+#async def fade():
+#    current_volume = pygame.mixer.music.get_volume()
+#    print('Fade happening...')
+#    while (fade_in_triggered):
+#        for i in range(int(current_volume) * 100, 100):
+#            pygame.mixer.music.set_volume(i/100)
+#            await asyncio.sleep(0.05)
+#    while (fade_out_triggered):
+#        for i in range(int(current_volume) * 100, 0):
+#            pygame.mixer.music.set_volume(i/100)
+#            await asyncio.sleep(0.05
 
 try:
     asyncio.run(main())
