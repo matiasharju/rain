488d563cbf4ff5243ebe602fb3822bffd3154b9d
Matias Harju <31343660+matiasharju@users.noreply.github.com>
2024-04-09 21:59:02 +0200

    add two sounds

--- pydubtest.py
+++ pydubtest.py
@@ -9,17 +9,18 @@ os.environ['SDL_ALSA_CARD'] = 'hw:2,0' # Check that the device number N is right
 from pydub import AudioSegment
 from pydub.playback import play
 
-backgroundMusic = AudioSegment.from_wav("sound1.wav")
+sound1 = AudioSegment.from_wav("sound1.wav")
+sound2 = AudioSegment.from_wav("sound2.wav")
 
-# pan the audio 15% to the right
-panned_right = backgroundMusic.pan(+1.0)
-
-# pan the audio 50% to the left
-panned_left = backgroundMusic.pan(-1.0)
+# pan audio to the left
+sound1Left = sound1.pan(-1.0)
+# pan audio to the right
+sound2Right = sound2.pan(+1.0)
 
 #Play audio
 while True:
     try:
-        play(panned_left)
+        play(sound1Left)
+        play(sound2Right)
     except KeyboardInterrupt:  # To handle Ctrl+C to exit the loop
         break
