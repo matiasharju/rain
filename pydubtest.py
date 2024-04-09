c843d669e78ce18427ea950e614834273be424ad
Matias Harju <31343660+matiasharju@users.noreply.github.com>
2024-04-09 22:11:58 +0200

    back to one sound

--- pydubtest.py
+++ pydubtest.py
@@ -10,17 +10,10 @@ from pydub import AudioSegment
 from pydub.playback import play
 
 sound1 = AudioSegment.from_wav("sound1.wav")
-sound2 = AudioSegment.from_wav("sound2.wav")
-
-# pan audio to the left
-sound1Left = sound1.pan(-1.0)
-# pan audio to the right
-sound2Right = sound2.pan(+1.0)
 
 #Play audio
 while True:
     try:
-        play(sound1Left)
-        play(sound2Right)
+        play(sound1)
     except KeyboardInterrupt:  # To handle Ctrl+C to exit the loop
         break
