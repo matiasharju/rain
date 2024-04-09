110358212e77b6f1e8148ce26fbe38be0a7df544
Matias Harju <31343660+matiasharju@users.noreply.github.com>
2024-04-09 21:54:13 +0200

    set environ variables

--- pydubtest.py
+++ pydubtest.py
@@ -1,3 +1,11 @@
+import os
+
+# Set environment variable for audio device before importing pydub
+os.environ['SDL_AUDIODRIVER'] = 'alsa'
+os.environ['SDL_ALSA_SETDMIXRATE'] = '48000'
+os.environ['SDL_ALSA_CARD'] = 'hw:2,0' # Check that the device number N is right (hw:N,0)!!!
+
+
 from pydub import AudioSegment
 from pydub.playback import play
 
@@ -9,12 +17,9 @@ panned_right = backgroundMusic.pan(+1.0)
 # pan the audio 50% to the left
 panned_left = backgroundMusic.pan(-1.0)
 
-panned_right.export("panned_right.wav", format="wav")
-panned_left.export("panned_left.wav", format="wav")
-
 #Play audio
 while True:
     try:
-        play(panned_left)
+        play(panned_right)
     except KeyboardInterrupt:  # To handle Ctrl+C to exit the loop
         break
