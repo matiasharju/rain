da072bec19def5dda837b05bc0d5967b0d17a428
Matias Harju <31343660+matiasharju@users.noreply.github.com>
2024-04-13 22:27:02 +0200

    a-right

--- a-right.py
+++ a-right.py
@@ -39,8 +39,33 @@ fade_out_triggered = None
 
 # **** MAIN COROUTINE ****
 async def main():
+    asyncio.create_task(fadeUp())
     while True:
        await asyncio.sleep(1) # to keep the main coroutine running
+
+# **** FADE UP ****
+async def fadeUp():
+    current_volume = pygame.mixer.music.get_volume()
+    print('Fade up happening...')
+    for i in range(int(current_volume * 100), 100, +1):
+        volume = i / 100
+        pygame.mixer.music.set_volume(volume)
+        await asyncio.sleep(0.05)
+        if volume >= 1.0:
+            break
+    print('Fade complete')
+
+# **** FADE DOWN ****
+async def fadeDown():
+    current_volume = pygame.mixer.music.get_volume()
+    print('Fade down happening...')
+    for i in range(int(current_volume * 100), 0, -1):
+        volume = i / 100
+        pygame.mixer.music.set_volume(volume)
+        await asyncio.sleep(0.05)
+        if volume <= 0.0:
+            break
+    print('Fade complete')
 
 try:
     asyncio.run(main())
