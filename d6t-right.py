31c9dcc9c7ed40a7e6138b0316a78ef57bfbcf4b
Matias Harju <31343660+matiasharju@users.noreply.github.com>
2024-04-15 18:08:15 +0200

    print debug when slower fade down

--- d6t-right.py
+++ d6t-right.py
@@ -182,12 +182,13 @@ async def measure():
             await asyncio.sleep(0.01)
 
         elif not values_over_threshold and pygame.mixer.music.get_volume() > 0.0:
-            print('Fade down happening...')
             if pygame.mixer.music.get_volume() > 0.05:
+                print('Fade down happening...')
                 current_volume = pygame.mixer.music.get_volume()
                 pygame.mixer.music.set_volume(current_volume - 0.02)
                 await asyncio.sleep(0.01)
             elif pygame.mixer.music.get_volume() <= 0.05:
+                print('Slower fade down happening...')
                 current_volume = pygame.mixer.music.get_volume()
                 pygame.mixer.music.set_volume(current_volume - 0.001)
                 await asyncio.sleep(0.01)
