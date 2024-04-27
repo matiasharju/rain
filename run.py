1202746f9ad2ee5b113cd838f537a480c71f10f3
Matias <matias.harju@iki.fi>
2024-04-27 13:09:56 +0300

    update run

--- run.py
+++ run.py
@@ -22,20 +22,16 @@ left_proc = start_subprocess(leftScript)
 right_proc = start_subprocess(rightScript)
 
 # Store the PIDs of the subprocesses
-subprocess_pids = [left_proc.pid, right_proc.pid]
+subprocess_pids = {leftScript: left_proc.pid, rightScript: right_proc.pid}
 
 try:
     # Continuous monitoring and restarting of subprocesses
     while True:
         # Check if each subprocess is still running, and restart if not
-        for pid in subprocess_pids:
+        for script, pid in subprocess_pids.items():
             if not is_process_running(pid):
-                if pid == left_proc.pid:
-                    left_proc = start_subprocess(leftScript)
-                    print("Left subprocess restarted.")
-                elif pid == right_proc.pid:
-                    right_proc = start_subprocess(rightScript)
-                    print("Right subprocess restarted.")
+                subprocess_pids[script] = start_subprocess(script).pid
+                print(f"{script} restarted.")
         # Adjust the interval based on your needs
         time.sleep(5)  # Check every 5 seconds
 except KeyboardInterrupt:
